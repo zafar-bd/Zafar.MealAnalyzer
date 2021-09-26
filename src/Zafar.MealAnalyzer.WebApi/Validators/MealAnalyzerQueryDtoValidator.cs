ef8447d84e125bbd4ddfd732af792626351bb236
Md Abu Zafar <mazafar.bd@gmail.com>
2021-09-26 11:53:54 +0600

    validator

---
+++ src/Zafar.MealAnalyzer.WebApi/Validators/MealAnalyzerQueryDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using Zafar.MealAnalyzer.Core.Models;
+
+namespace Zafar.MealAnalyzer.WebApi.Validators
+{
+    public class MealAnalyzerQueryDtoValidator : AbstractValidator<MealAnalyzerQueryDto>
+    {
+        public MealAnalyzerQueryDtoValidator()
+        {
+            RuleFor(d => d.StartDate)
+                 .NotEqual(DateTime.MinValue)
+                 .NotEqual(DateTime.MaxValue);
+
+            RuleFor(d => d.EndDate)
+                .NotEqual(DateTime.MinValue)
+                .NotEqual(DateTime.MaxValue);
+
+            RuleFor(d => d.EndDate)
+                .GreaterThanOrEqualTo(d => d.StartDate)
+                .WithMessage("End date must be greater than or equal start date");
+
+            RuleFor(d => d.UserType)
+                .IsInEnum()
+                .WithMessage("Invalid User Type");
+        }
+    }
+}
